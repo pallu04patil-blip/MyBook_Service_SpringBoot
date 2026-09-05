4a5ca32ad1cbb9817941a67742bb22ff40a087ac
pallu04patil-blip <pallu04patil@gmail.com>
2026-09-05 23:55:54 +0530

    Delete AuthorNotFoundException.java

--- AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookservice.exception;
-
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException(Long id) {
-        super("Author not found with id: " + id);
-    }
-}
